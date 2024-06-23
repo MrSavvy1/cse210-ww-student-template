cf7cb4820b92be7f03adeea453528c7582e93f2e
Saviour Edethu <108595932+MrSavvy1@users.noreply.github.com>
2024-06-23 02:35:44 +0000

    G

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,51 +1,20 @@
 using System;
 
-class SimpleFunctions
+class Program
 {
     static void Main(string[] args)
     {
         
-        DisplayWelcome();
+        first_name = input("What is your first name? ")
+
+       
+        last_name = input("What is your last name? ")
 
         
-        string userName = PromptUserName();
+        full_name = f"{last_name}, {first_name} {last_name}"
 
+        
+        print("test")
 
-        int favoriteNumber = PromptUserNumber();
-
-
-        int squaredNumber = SquareNumber(favoriteNumber);
-
-
-        DisplayResult(userName, squaredNumber);
-    }
-
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the program!");
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
-        return name;
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        return number;
-    }
-
-    static int SquareNumber(int number)
-    {
-        return number * number; 
-    }
-
-    static void DisplayResult(string name, int squaredNumber)
-    {
-        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
